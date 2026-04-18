25769e512992a1d53b63c7da09a746cc962b4ef8
AndreyAlKv <108435739+AndreyAlKv@users.noreply.github.com>
2026-04-19 02:46:35 +0300

    v2.2.6 LIVE cron SYSTEM DB 165 non-test query bat perfect

--- sdr_cycle.bat
+++ sdr_cycle.bat
@@ -1,11 +1,11 @@
 chcp 65001 >nul
 taskkill /f /im notepad.exe >nul 2>nul
 call .venv_local\Scripts\activate.bat
-echo SDR v2.2.4 START
+echo SDR v2.2.6 START
 python src\main.py
 python src\enrich.py
 python src\mailer.py
 python src\db_sync.py
 python src\db_query.py
 echo SDR END OK
-pausee
+pause
